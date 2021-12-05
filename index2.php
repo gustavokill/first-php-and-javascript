c677314690392e67a6785d3ae170fdd364341790
gustavokill <gacs111111@gmail.com>
2021-12-05 17:25:49 -0300

    PHP and Javascript

---
+++ index2.php
@@ -0,0 +1,45 @@
+<html>
+
+  <head> 
+    <title> 
+       Vamo pra cima timee !!! 
+    </title> 
+    
+	<script src="https://code.jquery.com/jquery-3.5.1.min.js" 
+	integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" 
+	crossorigin="anonymous">
+	
+	</script>
+	
+	
+	<style type="text/css">
+     .linha{
+		 font-weight: bold;
+		 color: green;
+		 padding-left: 60px;
+		 line-height: 40px;
+		    }	
+	</style>
+	
+	
+  </head>
+  
+  <body>
+   <?php
+       for ($i = 0; $i <= 10; $i++) 
+	 {
+	   print ("<span class=\"linha\">Linha número ". $i . "</span><br />");
+     }
+   ?> 
+  </body>
+  
+  <script type="text/javascript">
+   $(document).ready
+   (function()
+	 {
+	  alert("Isso é tudo p-pe-pessoal");
+	 }
+	);
+  </script>
+  
+</html>
